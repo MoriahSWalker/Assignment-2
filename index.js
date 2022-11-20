69eba10b4d53b58b6ea75e5b8ad1aae9b0613cf5
MoriahSWalker git config --global user.email riahrosebuds@gmail.com <you@example.com>
2022-11-20 15:45:41 -0500

    add problems 6-9

--- index.js
+++ index.js
@@ -22,3 +22,80 @@ let laps=0
 for (let laps = 1; laps <= 5; laps++) {
   console.log("that's another lap!" + (laps))
 }
+
+
+
+// 6. Make variable age. Make a loop from 0-100. In every loop check if i == age, if so log something, else say “age doesn’t match”.
+age=27
+  for (let i = 0; i < 100; i++) {
+    if (age == i) {
+      console.log((i) + "We have a match" 
+      )
+}
+    else {
+      console.log("age doesn't match")
+    }
+  }
+
+
+
+
+//7. Write a loop that logs all even numbers from 0-700.
+for (let i = 0; i <=700; i++) {
+ if (i % 2 == 0) {
+   console.log (i)}
+}
+
+
+//8. Write a loop that logs all odd numbers from 0-700.
+for (let i = 0; i < 700; i++) {
+  if (i % 2 > 0) {
+    console.log (i)
+  }
+  
+}
+
+
+
+//9. Write a loop with some “if else” statements. The loop should start at 1900 and go to 1949 and log when things were invented. For example when i = 1902 the code should log “it is 1902 - the teddy bear is invented!”
+timelineYears = [1900, 1902, 1910, 1913, 1918, 1923, 1935, 1938, 1943]
+for (let i = 0; i < timelineYears.length; i++) {
+  if (i == [0]) {
+    console.log('1900 - What is a "Zeppelin?"')
+  }
+  if (i == [1]) {
+    console.log( "1902 - Teddy Bear was Invented")
+    
+  }
+  if (i == [2]){
+    console.log("1910 - Talking Motion Picture")
+  }
+  if (i == [5]){
+    console.log("1923 - The Traffic Signal")
+  }
+  
+}
+
+
+
+//9a. another way of doing 
+timeline facts
+timelineYears = [1900, 1902, 1910, 1913, 1918, 1923, 1935, 1938, 1943]
+
+
+a = "1935 - The Radar was invented"
+b = "1938 - The Ballpoint pen was invented"
+c = "1943 - The Slinky was invented"
+for (let i = 0; i < timelineYears.length; i++){
+  if (i == [6]){
+    console.log(a)
+  }
+  if (i == [7]){
+    console.log(b)
+  }
+  if (i == [8]) {
+    console.log(c)
+    
+  }
+    
+}
