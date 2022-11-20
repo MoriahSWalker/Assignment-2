92a9f5aa1d99a798329ad6efae77d181fc64b64d
MoriahSWalker git config --global user.email riahrosebuds@gmail.com <you@example.com>
2022-11-20 15:41:12 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// // 2. Write a for loop that logs 0-15;
+// for (let i = 0; i <= 15; i++) {
+//   console.log(i)
+//   }
+
+
+
+
+
+// 3. Write a for loop that counts from 15-0
+for (let i = 15; i >= 0; i--) {
+  console.log(i)
+}
+
+
+
+
+
+
+// 4. Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
+let laps=0
+for (let laps = 1; laps <= 5; laps++) {
+  console.log("that's another lap!" + (laps))
+}
